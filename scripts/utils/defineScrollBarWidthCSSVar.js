2784154568d2a723b4f8d9c2cf119cb72eae5e3b
olehvitriachenko <olegvitracenko@gmail.com>
2025-06-07 17:09:15 +0300

    kl;k

--- scripts/utils/defineScrollBarWidthCSSVar.js
+++ scripts/utils/defineScrollBarWidthCSSVar.js
@@ -1,20 +1,20 @@
-// const defineScrollBarWidthCSSVar = () => {
-//     const outerElement = document.createElement('div')
-//     outerElement.style.visibility = 'hidden'
-//     outerElement.style.overflow = 'scroll'
+const defineScrollBarWidthCSSVar = () => {
+    const outerElement = document.createElement('div')
+    outerElement.style.visibility = 'hidden'
+    outerElement.style.overflow = 'scroll'
 
-//     document.body.append(outerElement)
+    document.body.append(outerElement)
 
-//     const innerElement = document.createElement('div')
+    const innerElement = document.createElement('div')
 
-//     outerElement.append(innerElement)
+    outerElement.append(innerElement)
 
-//     document.documentElement.style.setProperty(
-//         '--scrollbar-width',
-//         `${outerElement.offsetWidth - innerElement.offsetWidth}px`
-//     )
+    document.documentElement.style.setProperty(
+        '--scrollbar-width',
+        `${outerElement.offsetWidth - innerElement.offsetWidth}px`
+    )
 
-//     outerElement.remove()
-// }
+    outerElement.remove()
+}
 
-// export default defineScrollBarWidthCSSVar
+export default defineScrollBarWidthCSSVar
